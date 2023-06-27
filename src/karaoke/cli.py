6599cc00991dc8d9cdf0ed7b62a3d8497de55fad
Amir Rachum <amir@rachum.com>
2023-06-27 15:00:10 +0300

    .

--- src/karaoke/cli.py
+++ src/karaoke/cli.py
@@ -1,14 +1,25 @@
-import click
+from typing import Optional
 
+import click
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from karaoke.core.user import User
 from karaoke.core.song import Song
 from karaoke.core.rating import UserSongRating, Rating
-from karaoke.core.session import Session, SessionSong, SessionUser
+from karaoke.core.session import (
+    KaraokeSession,
+    KaraokeSessionSong,
+    KaraokeSessionUser,
+)
 from karaoke.core.base import Base
+import logging
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
 
 LOCAL_DB = "sqlite:///karaoke.sqlite"
 
@@ -28,14 +39,142 @@ def _init_db() -> None:
 
 
 @click.command()
-def _create_user() -> None:
+@click.option("--name", "-n", type=str, help="Name")
+def _create_user(name: Optional[str]) -> None:
     engine = create_engine(LOCAL_DB, echo=True)
     with sessionmaker(bind=engine)() as session:
-        name = click.prompt("Name")
+        if name is None:
+            name = click.prompt("Name")
         user = User(name=name)
         session.add(user)
         session.commit()
         click.echo(f"User created with ID {user.id}")
+
+
+@click.command()
+def _list_users() -> None:
+    engine = create_engine(LOCAL_DB, echo=True)
+    with sessionmaker(bind=engine)() as session:
+        all_users = session.query(User).all()
+        click.echo(f"Found {len(all_users)} users:")
+        for user in all_users:
+            click.echo(f"{user.id}: {user.name}")
+
+
+@click.command()
+@click.option("--user-id", "-u", type=int, help="User ID")
+def _rate_song(user_id: int) -> None:
+    engine = create_engine(LOCAL_DB, echo=False)
+    with sessionmaker(bind=engine)() as session:
+        user: Optional[User] = (
+            session.query(User).filter_by(id=user_id).first()
+        )
+        if user is None:
+            click.echo(f"User with ID {user_id} not found.")
+            return
+
+        ratings: dict[int, Rating] = {
+            0: Rating.DONT_KNOW,
+            1: Rating.SING_ALONG,
+            2: Rating.CAN_TAKE_THE_MIC,
+            3: Rating.NEED_THE_MIC,
+        }
+        rating_names: dict[Rating, str] = {
+            Rating.DONT_KNOW: "I don't know the song",
+            Rating.SING_ALONG: "I can sing along",
+            Rating.CAN_TAKE_THE_MIC: "I can take the mic",
+            Rating.NEED_THE_MIC: "I NEED the mic!",
+        }
+
+        while (
+            unrated_song := (
+                session.query(Song)
+                .outerjoin(KaraokeSessionSong)
+                .where(KaraokeSessionSong.song_id == None)
+                .first()
+            )
+        ) is not None:
+            click.echo(f"How well do you know this song?")
+            click.echo(format_song(unrated_song))
+            click.echo()
+            for score, rating in sorted(ratings.items()):
+                click.echo(f"{score}: {rating_names[rating]}")
+            score = click.prompt("Score", type=int)
+            user_song_rating = UserSongRating(
+                user_id=user.id, song_id=unrated_song.id, rating=ratings[score]
+            )
+            session.add(user_song_rating)
+            session.commit()
+            click.echo()
+
+
+@click.command()
+@click.option("--title", "-t", type=str, help="Title")
+@click.option("--artist", "-a", type=str, help="Artist")
+@click.option("--video-link", "-l", type=str, help="Video Link")
+def _create_song(title, artist, video_link) -> None:
+    engine = create_engine(LOCAL_DB, echo=True)
+    with sessionmaker(bind=engine)() as session:
+        if title is None:
+            title = click.prompt("Title")
+        if artist is None:
+            artist = click.prompt("Artist")
+        if video_link is None:
+            video_link = click.prompt("Video Link")
+        song = Song(title=title, artist=artist, video_link=video_link)
+        session.add(song)
+        session.commit()
+        click.echo(f"Song created with ID {song.id}")
+
+
+@click.command()
+def _list_songs() -> None:
+    engine = create_engine(LOCAL_DB, echo=True)
+    with sessionmaker(bind=engine)() as session:
+        all_songs = session.query(Song).all()
+        click.echo(f"Found {len(all_songs)} songs:")
+        for song in all_songs:
+            click.echo(f"{song.id}: {format_song(song)}")
+
+
+@click.command()
+@click.option("--user-id", "-u", type=int, help="User ID", multiple=True)
+def _create_session(user_id: list[int]) -> None:
+    engine = create_engine(LOCAL_DB, echo=False)
+    with sessionmaker(bind=engine)() as session:
+        karaoke_session = KaraokeSession()
+        karaoke_session.generate_display_id(session)
+        session.add(karaoke_session)
+        session.commit()
+
+        for uid in user_id:
+            session_user = KaraokeSessionUser(
+                karaoke_session_id=karaoke_session.id, user_id=uid
+            )
+            session.add(session_user)
+
+        session.commit()
+        karaoke_session.generate_song_queue(session)
+        click.echo(f"Session created with ID {karaoke_session.display_id}")
+
+
+@click.command()
+@click.option("--session-id", "-s", type=str, help="Session ID")
+def next_song(session_id: str) -> None:
+    engine = create_engine(LOCAL_DB, echo=True)
+    with sessionmaker(bind=engine)() as session:
+        karaoke_session = (
+            session.query(KaraokeSession).filter(display_id=session_id).first()
+        )
+        if karaoke_session is None:
+            click.echo("Session not found")
+            return
+
+        song: Optional[Song] = karaoke_session.get_next_song()
+        if song is None:
+            click.echo("No more songs in the queue.")
+            return
+        click.echo(f"Next song: {format_song(song)}")
 
 
 @click.group()
@@ -53,12 +192,35 @@ def _user() -> None:
     pass
 
 
+@click.group()
+def _song() -> None:
+    pass
+
+
+@click.group()
+def _session() -> None:
+    pass
+
+
 _cli.add_command(_db, name="db")
 _db.add_command(_init_db, name="init")
 
 _cli.add_command(_user, name="user")
 _user.add_command(_create_user, name="create")
+_user.add_command(_list_users, name="list")
+_user.add_command(_rate_song, name="rate")
+
+_cli.add_command(_song, name="song")
+_song.add_command(_create_song, name="create")
+_song.add_command(_list_songs, name="list")
+
+_cli.add_command(_session, name="session")
+_session.add_command(_create_session, name="create")
+
+
+def main():
+    _cli()
 
 
 if __name__ == "__main__":
-    _cli()
+    main()
--- src/karaoke/core/session.py
+++ src/karaoke/core/session.py
@@ -1,35 +1,33 @@
-from sqlalchemy import ForeignKey, String, Integer, Column, Table
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, select
+from sqlalchemy.orm import Mapped, mapped_column, relationship, Session
 from enum import Enum
+import random
+import logging
 
 from karaoke.core.base import Base
+from karaoke.core.rating import UserSongRating
+from karaoke.core.song import Song
+
+logger = logging.getLogger(__name__)
 
 
-session_user_association_table = Table(
-    "session_user_association",
-    Base.metadata,
-    Column("session", String(4), ForeignKey("session.session_id")),
-    Column("user", Integer, ForeignKey("user_account.id")),
-)
+class KaraokeSessionUser(Base):
+    __tablename__ = "karaoke_session_user"
 
-
-class SessionUser(Base):
-    __tablename__ = "session_user"
-
-    session_id: Mapped[str] = mapped_column(
-        ForeignKey("session.session_id"), primary_key=True
+    karaoke_session_id: Mapped[str] = mapped_column(
+        ForeignKey("karaoke_session.id"), primary_key=True
     )
     user_id: Mapped[int] = mapped_column(
         ForeignKey("user_account.id"), primary_key=True
     )
-    score: Mapped[int] = mapped_column()
+    score: Mapped[int] = mapped_column(default=0)
 
 
-class SessionSong(Base):
-    __tablename__ = "session_song"
+class KaraokeSessionSong(Base):
+    __tablename__ = "karaoke_session_song"
 
-    session_id: Mapped[str] = mapped_column(
-        ForeignKey("session.session_id"), primary_key=True
+    karaoke_session_id: Mapped[str] = mapped_column(
+        ForeignKey("karaoke_session.id"), primary_key=True
     )
     song_id: Mapped[int] = mapped_column(
         ForeignKey("song.id"), primary_key=True
@@ -37,9 +35,41 @@ class SessionSong(Base):
     played: Mapped[bool] = mapped_column()
 
 
-class Session(Base):
-    __tablename__ = "session"
+def generate_id() -> str:
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return "".join(random.choices(letters, k=4))
 
-    session_id: Mapped[str] = mapped_column(String(4), primary_key=True)
-    users: Mapped[list[SessionUser]] = relationship()
-    songs: Mapped[list[SessionSong]] = relationship()
+
+class KaraokeSession(Base):
+    __tablename__ = "karaoke_session"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    display_id: Mapped[str] = mapped_column(
+        String(4), nullable=True, unique=True
+    )
+    users: Mapped[list[KaraokeSessionUser]] = relationship()
+    songs: Mapped[list[KaraokeSessionSong]] = relationship()
+
+    def generate_display_id(self, session: Session):
+        display_id = generate_id()
+        all_sessions = session.query(KaraokeSession).all()
+        all_display_ids = [ks.display_id for ks in all_sessions]
+        while display_id in all_display_ids:
+            display_id = generate_id()
+        self.display_id = display_id
+
+    def generate_song_queue(self, session: Session):
+        user_ids = [user.user_id for user in self.users]
+        result = session.execute(
+            select(Song).where(
+                Song.id.in_(
+                    select(UserSongRating.song_id)
+                    .select_from(UserSongRating)
+                    .where(UserSongRating.user_id.in_(user_ids))
+                )
+            )
+        )
+        logger.info(result.all())
+
+    # def get_next_song(self, session: Session):
+    # self.songs.filter(played=False)
